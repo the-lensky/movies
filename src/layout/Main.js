03d009a08a40ae5307c44cb378d88403bf47c7d3
the-lensky <the-lenskye@users.noreply.github.com>
2021-04-30 15:56:04 +0300

    test3

--- src/layout/Main.js
+++ src/layout/Main.js
@@ -43,6 +43,7 @@ const Main = () => {
 
     return (
         <main className='container content'>
+            <h1>test3</h1>
             <Search searchMovies={searchMovies}/>
             {
                 loading ? <Preloader/> : <Movies movies={movies}/>
