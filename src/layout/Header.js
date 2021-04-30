acdd268fcd9cff3bc77a7adce6f1c22c4ddf652b
ravesnature <76727818+ravesnature@users.noreply.github.com>
2021-04-30 15:23:03 +0300

    -add github link

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
             <div className="nav-wrapper">
                 <a href="#" className="brand-logo">REACT MOVIES</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="!#">Repo</a></li>
+                    <li><a href="https://github.com/the-lensky/movies" target="_blank">Repo</a></li>
                 </ul>
             </div>
         </nav>
