0ff260c9a492cfd3080197b6be3c6d74f7bd448e
the-lensky <55101239+the-lensky@users.noreply.github.com>
2021-05-08 14:46:49 +0300

    -fix branch[3]

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <nav className='purple'>
             <div className="nav-wrapper">
-                <a href="https://github.com/the-lensky/movies" rel="noreferrer" target="_blank" className="brand-logo">REACT MOVIES</a>
+                <a href='/' className="brand-logo">REACT MOVIES</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li><a href="https://github.com/the-lensky/movies" rel="noreferrer" target="_blank">Repo</a></li>
                 </ul>
