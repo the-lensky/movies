294dd33593571b708c7649c873ff4607ace6b001
the-lensky <55101239+the-lensky@users.noreply.github.com>
2021-05-08 14:59:25 +0300

    -fix search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const Search = ({searchMovies}) => {
 
-    const [search, setSearch] = useState('mortal kombat')
+    const [search, setSearch] = useState('')
     const [filter, setFilter] = useState('all')
 
     const handleKey = (e) => {
