d98a45a1a9efeec274a306915822a9fae0e93066
the-lensky <55101239+the-lensky@users.noreply.github.com>
2021-05-08 14:34:09 +0300

    -fix header and footer

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App(props) {
     return (
         <>
             <Header/>
-            <Main />
-            <Footer />
+            <Main/>
+            <Footer/>
         </>
     )
 }
