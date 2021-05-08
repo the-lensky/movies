e0159277ad4b96afd810f19566f42ef3e0c8646d
the-lensky <55101239+the-lensky@users.noreply.github.com>
2021-05-08 14:44:00 +0300

    -fix branch[2]

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -3,10 +3,9 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="page-footer purple lighten-1">
-            <div className="footer-copyright purple lighten-1">
+            <div className="footer-copyright purple lighten-1 center">
                 <div className="container">
-                    © { new Date().getFullYear()} Have a nice day!😼
-                    <a className="grey-text text-lighten-4 right" href="https://github.com/the-lensky/movies" target="_blank">Repo</a>
+                    © {new Date().getFullYear()} Have a nice day!😼
                 </div>
             </div>
         </footer>
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
     return (
         <nav className='purple'>
             <div className="nav-wrapper">
-                <a href="#" className="brand-logo">REACT MOVIES</a>
+                <a href="https://github.com/the-lensky/movies" rel="noreferrer" target="_blank" className="brand-logo">REACT MOVIES</a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><a href="https://github.com/the-lensky/movies" target="_blank">Repo</a></li>
+                    <li><a href="https://github.com/the-lensky/movies" rel="noreferrer" target="_blank">Repo</a></li>
                 </ul>
             </div>
         </nav>
